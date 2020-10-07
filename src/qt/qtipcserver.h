e0a4e668e4b39bc17efb696119514b0cadae47fb
gmsilva35 <mmngeorge@hotmail.com>
2020-10-07 19:35:08 -0300

    init

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define betscoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "betscoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
